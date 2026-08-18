b8c5835b317cde3bad44a7911c25f74cfd1fda53
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-08-18 14:58:56 +0100

    remove redundant parameters

--- terraform/auth0/clients.tf
+++ terraform/auth0/clients.tf
@@ -62,7 +62,6 @@ resource "auth0_client_credentials" "auth0_actions_management_client" {
 }
 
 resource "auth0_client_grant" "auth0_actions_management_client_grant" {
-  allow_all_scopes            = false
   allow_any_organization      = false
   audience                    = "https://${var.auth0_domain}/api/v2/"
   client_id                   = auth0_client.auth0_actions_management_client.client_id

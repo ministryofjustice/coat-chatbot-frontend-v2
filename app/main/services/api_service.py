a36a94c7a056e178ea4e09d612bef475e53ffda6
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-08-19 12:21:37 +0100

    inject app env into deployment

--- app/main/services/api_service.py
+++ app/main/services/api_service.py
@@ -2,13 +2,14 @@ import requests
 
 class api_service:
     def __init__(
-        self, api_id: str, api_key: str
+        self, api_id: str, api_key: str, app_env: str
     ) -> None:
         self.api_id = api_id
         self.api_key = api_key
+        self.app_env = app_env
 
     def submit_query(self, query):
-        url = f"https://{self.api_id}.execute-api.eu-west-2.amazonaws.com/production/send-request"
+        url = f"https://{self.api_id}.execute-api.eu-west-2.amazonaws.com/{self.app_env}/send-request"
 
         payload = {"user_question": query}
 
